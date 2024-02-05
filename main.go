41260ae49f176eac97aaed46af12f1d9387c7dca
johnche <john20chen@gmail.com>
2024-02-05 16:31:12 +0100

    testing stuff

--- main.go
+++ main.go
@@ -8,9 +8,19 @@ import (
 )
 
 func main() {
-	ebiten.SetWindowSize(500, 500)
+	width := 1000
+	height := 1000
+
+	newGame := game.New(
+		game.WithWidth(width),
+		game.WithHeight(height),
+		game.WithTPS(20),
+	)
+
+	ebiten.SetWindowSize(width, height)
 	ebiten.SetWindowTitle("Snake")
-	if err := ebiten.RunGame(game.New()); err != nil {
+
+	if err := ebiten.RunGame(newGame); err != nil {
 		log.Fatalf("failed running game: %v", err)
 	}
 }
